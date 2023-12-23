d4a11c2c812d5ee2d5dc92e344ec7ec7bf78d408
Ben Zhou <you@example.com>
2023-12-23 23:35:52 +0800

    Task 2 completed

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import './App.css';
  */
 interface IState {
   data: ServerRespond[],
+  showGraph: boolean, // add a new state variable to indicate whether the graph should be shown
 }
 
 /**
@@ -22,6 +23,8 @@ class App extends Component<{}, IState> {
       // data saves the server responds.
       // We use this state to parse data down to the child element (Graph) as element property
       data: [],
+      // initialize the state variable to false
+      showGraph: false,
     };
   }
 
@@ -29,18 +32,32 @@ class App extends Component<{}, IState> {
    * Render Graph react component with state.data parse as property data
    */
   renderGraph() {
-    return (<Graph data={this.state.data}/>)
+    // if showGraph is true, render the Graph component
+    if (this.state.showGraph) {
+      return (<Graph data={this.state.data}/>)
+    }
   }
 
   /**
    * Get new data from server and update the state with the new data
    */
   getDataFromServer() {
-    DataStreamer.getData((serverResponds: ServerRespond[]) => {
-      // Update the state by creating a new array of data that consists of
-      // Previous data in the state and the new data from server
-      this.setState({ data: [...this.state.data, ...serverResponds] });
-    });
+    let i = 0;
+    const interval = setInterval(() => {
+
+      DataStreamer.getData((serverResponds: ServerRespond[]) => {
+        // Update the state by creating a new array of data that consists of
+        // Previous data in the state and the new data from server
+        this.setState({ data: [...this.state.data, ...serverResponds], showGraph: true });
+      });
+      // increment i by 1
+      i++;
+      // set showGraph to true
+      // if i is greater than 1000, stop the interval
+      if (i > 1000) {
+        clearInterval(interval);
+      }
+    }, 100);
   }
 
   /**
